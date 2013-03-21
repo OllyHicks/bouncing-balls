de03f3c1a3e0e8c3645ecf64b6eeb9033ac58dd7
Olly Hicks <me@ollyhicks.co.uk>
2013-03-21 16:01:36 +0000

    Started on ball collision programming

--- simulator.py
+++ simulator.py
@@ -37,6 +37,7 @@ class Simulator:
     num_balls = 8
     dist_matrix = []
     
+    _prev_collisions = []
     
     def __init__(self, window):
         self.batch = pyglet.graphics.Batch()
@@ -77,8 +78,13 @@ class Simulator:
         for ball in self.balls:
             ball.update(dt)
         
-        for collision in self.detect_all_collisions():
-            self.simulat_ball_collision(collision[0], collision[1])
+        collisions = self.detect_all_collisions()
+        
+        for collision in collisions:
+            if not collision in self._prev_collisions:
+                self.simulat_ball_collision(collision[0], collision[1])
+            
+        self._prev_collisions = collisions
     
     
     def detect_collision(self, ball, other_balls=None):
@@ -110,5 +116,28 @@ class Simulator:
         return collisions
     
     def simulat_ball_collision(self, ball_1, ball_2):
-        ball_1.vx = ball_1.vy = 0
-        ball_2.vx = ball_2.vy = 0
+        #ball_1.vx = ball_1.vy = 0
+        #ball_2.vx = ball_2.vy = 0
+        
+        normal_line = math.atan((ball_2.y - ball_1.y)/(1.0*(ball_2.x- ball_1.x)))
+        
+        print normal_line
+        
+        # Rotate direction
+        ball_1_vn = ball_1.vx * math.cos(normal_line) - ball_1.vy * math.sin(normal_line)
+        ball_1_vt = ball_1.vy * math.cos(normal_line) + ball_1.vx * math.sin(normal_line)
+        
+        ball_2_vn = ball_2.vx * math.cos(normal_line) - ball_2.vy * math.sin(normal_line)
+        ball_2_vt = ball_2.vy * math.cos(normal_line) + ball_2.vx * math.sin(normal_line)
+        
+        # Do collision
+        #ball_1_vn = -1 * ball_1_vn
+        #ball_2_vn = -1 * ball_2_vn
+        ball_1_vn, ball_2_vn = ball_2_vn, ball_1_vn
+        
+        # Rotate back
+        ball_1.vx = ball_1_vn * math.cos(-normal_line) - ball_1_vt * math.sin(-normal_line)
+        ball_1.vy = ball_1_vt * math.cos(-normal_line) + ball_1_vn * math.sin(-normal_line)
+        
+        ball_2.vx = ball_2_vn * math.cos(-normal_line) - ball_2_vt * math.sin(-normal_line)
+        ball_2.vy = ball_2_vt * math.cos(-normal_line) + ball_2_vn * math.sin(-normal_line)
